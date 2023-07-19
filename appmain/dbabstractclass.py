2fbf83a63b09bfdd04e04fe5db763df344f735ab
RShibuya <paw27182@gmail.com>
2023-07-19 13:44:28 +0900

    miscellaneous

--- appmain/dbabstractclass.py
+++ appmain/dbabstractclass.py
@@ -8,20 +8,51 @@ class DBAbstractClass(metaclass=ABCMeta):
 
     @abstractmethod
     def get_form_size(self, selected_form=None):
+        """Find the number of form records
+
+        Args:
+            selected_form (str): the form table name
+
+        Returns:
+            int: the number of form records
+        """
         return 0
 
     @abstractmethod
     def write_form_items(self, input_data):
-        return "OK"
+        """Write form items into the table
+
+        Args:
+            input_data (list of list): the list of form items
+
+        Returns:
+            tuple of str: the return messages
+        """
+        return "OK", "Done."
 
     @abstractmethod
     def read_form_items(self, command):
+        """Read form items from the table
+
+        Args:
+            command (str): command name, e.g.) 'show_the_first_item'
+
+        Returns:
+            tuple of lists: the list of table key names and the list of form items
+        """
         return [], []  # key, items
 
     @abstractmethod
     def drop_form(self):
+        """Delete the form table
+        """
         pass
 
     @abstractmethod
     def delete_item(self, record_to_be_processed):
+        """Delete the form record
+
+        Args:
+            record_to_be_processed (str): delete condition
+        """
         pass
